028de1d845e7acc64c7d0685ed23baa7c508c4ad
displaycoffee <adria.m.murphy@gmail.com>
2025-11-23 12:00:59 -0700

    cleaning up

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 3000,
 		strictPort: true,
-		origin: `${'https://wordpress.ddev.site'.replace(/:\d+$/, '')}:3000`,
+		origin: `${'https://memoria.ddev.site'.replace(/:\d+$/, '')}:3000`,
 		cors: {
 			origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.ddev\.site)(?::\d+)?$/,
 		},
